a4ee628b7420f0ff132f13bce1756d0bd3e82262
piter1316 <piter_1316@wp.pl>
2019-11-18 12:18:07 +0100

    setting split for production and develop + databases set up

---
+++ settings/development.py
@@ -0,0 +1,14 @@
+from .common import *
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '2U0rOLExRZ',
+        'USER': '2U0rOLExRZ',
+        'PASSWORD': 'tORNQuOeuQ',
+        'HOST': 'remotemysql.com',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+    }
+}
---
+++ settings/production.py
@@ -0,0 +1,14 @@
+from .common import *
+
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mQEFKcmB7i',
+        'USER': 'mQEFKcmB7i',
+        'PASSWORD': 'bP5U2mqHqM',
+        'HOST': 'remotemysql.com',  # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+    }
+}
