b7185052576f73a32b342eefc22f9281509bf006
piter1316 <piter_1316@wp.pl>
2019-11-19 11:25:16 +0100

    separate todo for users

--- todo/models.py
+++ todo/models.py
@@ -1,9 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class Todo(models.Model):
     text = models.CharField(max_length=60)
     complete = models.BooleanField(default=False)
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.text
--- todo/views.py
+++ todo/views.py
@@ -7,7 +7,7 @@ from django.views.decorators.http import require_POST
 
 @login_required(login_url='/accounts/login')
 def index(request):
-    todo_list = Todo.objects.order_by('id')
+    todo_list = Todo.objects.filter(user=request.user).order_by('id')
 
     form = TodoForm()
     context = {'todo_list': todo_list, 'form': form}
@@ -19,10 +19,10 @@ def addTodo(request):
     form = TodoForm(request.POST)
 
     if form.is_valid():
-        new_todo = Todo(text=request.POST['text'])
+        new_todo = Todo(text=request.POST['text'], user=request.user)
         new_todo.save()
 
-    return redirect('index')
+    return redirect('todo:index')
 
 
 def completeTodo(request, todo_id):
@@ -30,18 +30,18 @@ def completeTodo(request, todo_id):
     todo.complete = True
     todo.save()
 
-    return redirect('index')
+    return redirect('todo:index')
 
 
 def deleteCompleted(request):
-    Todo.objects.filter(complete__exact=True).delete()
+    Todo.objects.filter(complete__exact=True, user=request.user).delete()
 
-    return redirect('index')
+    return redirect('todo:index')
 
 
 def deleteAll(request):
-    Todo.objects.all().delete()
-    return redirect('index')
+    Todo.objects.filter(user=request.user).delete()
+    return redirect('todo:index')
 
 
 def login(request):
