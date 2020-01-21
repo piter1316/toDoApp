e0282fc6522c837d3eadd9ce963216bb95623aca
piter1316 <piter_1316@wp.pl>
2020-01-21 11:27:33 +0100

    draft car model

--- cars/views.py
+++ cars/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from cars.models import Car
+
+
 def cars_home(request):
-    context = {}
+    user_cars = Car.objects.filter(user=request.user)
+    context = {
+        'user_cars': user_cars
+    }
     return render(request, 'cars/home.html', context)
--- myproject/urls.py
+++ myproject/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from django.views.static import serve
 
+from myproject import settings
 from todo import views
 
 urlpatterns = [
@@ -28,4 +31,6 @@ urlpatterns = [
     path('', include('shopping.urls')),
     path('', include('meals.urls')),
     path('', include('cars.urls')),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+
 ]
