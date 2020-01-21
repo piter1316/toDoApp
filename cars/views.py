993a402859f0fc0989855facd871028eea5274e9
piter1316 <piter_1316@wp.pl>
2020-01-21 11:46:35 +0100

    draft car details

--- cars/views.py
+++ cars/views.py
@@ -11,3 +11,11 @@ def cars_home(request):
         'user_cars': user_cars
     }
     return render(request, 'cars/home.html', context)
+
+
+def car_details(request, car_id):
+    car = Car.objects.filter(pk=car_id, user=request.user)
+    context = {
+        'car': car
+    }
+    return render(request, 'cars/car_details.html', context)
