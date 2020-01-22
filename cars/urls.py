b3c15e9497d499ff3e9c2c04df5b154b3457c0a5
piter1316 <piter_1316@wp.pl>
2020-01-22 15:55:43 +0100

    car edit draft

--- cars/urls.py
+++ cars/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('cars', views.cars_home, name='cars_home'),
     path('cars/addCar', views.add_new_car, name='add_new_car'),
     path('cars/carDetails/<car_id>', views.car_details, name='car_details'),
+    path('cars/carDetails/<pk>/edit', views.CarUpdate.as_view(), name='car_edit'),
 
 ]
--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
+from django.urls import reverse, reverse_lazy
+from django.views.generic import UpdateView
+
 from cars import forms
-from cars.forms import NewCarForm
+from cars.forms import NewCarForm, UpdateCar
 from cars.models import Car
 
 
@@ -18,11 +21,25 @@ def cars_home(request):
 
 def car_details(request, car_id):
     car = Car.objects.filter(pk=car_id, user=request.user)
+    form = UpdateCar(request.POST, request.FILES, )
     context = {
-        'car': car
+        'car': car,
+        'new_car_form': form,
     }
     return render(request, 'cars/car_details.html', context)
 
+
+class CarUpdate(UpdateView):
+    model = Car
+    form_class = UpdateCar
+
+    def get_success_url(self):
+        pk = self.object.pk
+        return '{}'.format(reverse('cars:car_details', kwargs={'car_id': pk}))
+
+
+    template_name = 'cars/car_edit.html'
+    # success_url = redirect('cars:car_details', Car.pk)
 
 # def add_new_car(request):
 #     print('new car')
