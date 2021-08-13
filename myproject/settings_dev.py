c3795d5e8f50c87e6fc5f727e0ae76dfbcc7ddb4
piter <pdpiatek@gmail.com>
2021-08-13 14:35:21 +0200

    jadlospis ajax post with average calories refresh

--- myproject/settings_dev.py
+++ myproject/settings_dev.py
@@ -79,23 +79,23 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db_dev.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ilDKCpjM48',
-        'USER': 'ilDKCpjM48',
-        'PASSWORD': 'RmnQAxrmtw',
-        'HOST': 'remotemysql.com',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db_dev.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'ilDKCpjM48',
+#         'USER': 'ilDKCpjM48',
+#         'PASSWORD': 'RmnQAxrmtw',
+#         'HOST': 'remotemysql.com',   # Or an IP Address that your DB is hosted on
+#         'PORT': '3306',
+#     }
+# }
 
 
 # Password validation
